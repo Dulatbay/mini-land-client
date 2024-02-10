ad9a882d5da9f59e269c713d78cd397ce891d31f
Dulatbay <adulatbai@bk.ru>
2024-02-10 17:59:28 +0600

    add: create DirectorMasterClassesPage.tsx

--- src/2_pages/Director/DirectorMasterClassesPage/DirectorMasterClassesPage.tsx
+++ src/2_pages/Director/DirectorMasterClassesPage/DirectorMasterClassesPage.tsx
@@ -1,3 +1,25 @@
+import {ButtonAddItem} from "@/6_shared/BaseComponents/ButtonAddItem";
+import {useNavigate} from "react-router-dom";
+import {MasterClassCard} from "@/5_entities/masterClass/ui/MasterClassCard.tsx";
+
 export const DirectorMasterClassesPage = () => {
-    return <></>
+    const navigate = useNavigate()
+    const createClickHandler = () => {
+        navigate("/create-master-class")
+    }
+
+    return <div className={'w-[95%] m-auto'}>
+        <div className={`flex justify-between items-center pt-7`}>
+            <div className={`text-[24px] md:text-[30px]`}>
+                Ваши мастер классы
+            </div>
+            <ButtonAddItem text={"Создать мастер класс"} clickHandler={createClickHandler}/>
+        </div>
+        <div className={'flex flex-wrap gap-4 mt-2'}>
+            <MasterClassCard/>
+            <MasterClassCard/>
+            <MasterClassCard/>
+            <MasterClassCard/>
+        </div>
+    </div>
 }
