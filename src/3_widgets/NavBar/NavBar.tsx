2884312a35333b3b12370ae901a7ea7d9bd607e6
Dulatbay <adulatbai@bk.ru>
2024-02-11 10:30:53 +0600

    fix: remove sticky param from navbar

--- src/3_widgets/NavBar/NavBar.tsx
+++ src/3_widgets/NavBar/NavBar.tsx
@@ -48,7 +48,7 @@ export const NavBar = () => {
     );
     return (
         <Navbar
-            className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-slate-900 border-none"
+            className="h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-slate-900 border-none"
             placeholder={""}>
             <div className="flex items-center justify-between text-blue-gray-900">
                 <Typography
