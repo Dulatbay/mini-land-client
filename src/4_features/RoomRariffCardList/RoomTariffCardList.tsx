9810475728fa157e8c1db5d29b71ec9098b092cc
Dulatbay <adulatbai@bk.ru>
2024-02-11 10:21:56 +0600

    add: create room tariff processes for manager

--- src/4_features/RoomRariffCardList/RoomTariffCardList.tsx
+++ src/4_features/RoomRariffCardList/RoomTariffCardList.tsx
@@ -1,11 +1,26 @@
+import {useAllRoomTariffsQuery} from "@/5_entities/roomTariff/api/roomTariffApi.ts";
+import {useEffect} from "react";
+import {getToastMessage} from "@/6_shared/lib/getToastMessage.ts";
+import {Spinner} from "@/6_shared/BaseComponents/Spinner/Spinner.tsx";
 import {RoomTariffCard} from "@/5_entities/roomTariff/ui/RoomTariffCard.tsx";
 
 export const RoomTariffCardList = () => {
+    const {data, isError, error, isLoading} = useAllRoomTariffsQuery(true)
+
+    useEffect(() => {
+        if (isError)
+            getToastMessage(error)
+    }, [isError, error]);
+
+
+    if (isLoading)
+        return <Spinner/>
+
     return <div className={'flex flex-wrap mt-2'}>
-        <RoomTariffCard/>
-        <RoomTariffCard/>
-        <RoomTariffCard/>
-        <RoomTariffCard/>
-        <RoomTariffCard/>
+        {
+            data!.length ?
+                data!.map(i => <RoomTariffCard {...i} />) :
+                <p className={'text-gray-700'}>Создайте первый тариф</p>
+        }
     </div>
 }
--- src/5_entities/roomTariff/ui/RoomTariffCard.tsx
+++ src/5_entities/roomTariff/ui/RoomTariffCard.tsx
@@ -1,30 +1,43 @@
-import {daysOfWeek} from "@/6_shared/lib/daysOfWeek.ts";
+import {DaysOfWeek} from "@/4_features/DaysOfWeek/DaysOfWeek.tsx";
 
-export const RoomTariffCard = () => {
+interface Props {
+    id: number;
+    started_time: string;
+    ended_time: string;
+    first_price: number;
+    max_child: number;
+    week_days: number[];
+}
+const getSelectedDays = (weekDays: number[]) => {
+    return [1, 2, 3, 4, 5, 6, 7].map((i) => weekDays.includes(i))
+}
+export const RoomTariffCard = ({started_time, ended_time, first_price, max_child, week_days}: Props) => {
+
+
+
     return <div className={`bg-white  min-h-40 rounded-2xl flex flex-col justify-between px-4 py-5 border-2`}>
         <div className={'w-full flex flex-wrap justify-between'}>
-            <p className={'text-xl'}>12:00-15:30</p>
+            <p className={'text-xl'}>{started_time}-{ended_time}</p>
             <div className={`text-right`}>
-                <p className={'text-xl leading-none'}>45000тг</p>
-                <p>Цена до 6 детей</p>
+                <p className={'text-xl leading-none'}>{first_price}тг</p>
+                <p>Цена до {max_child} детей</p>
             </div>
         </div>
         <div className={'w-full'}>
-            <div className={'flex flex-wrap gap-1'}>
-                {
-                    daysOfWeek.map((i, j) => <div
-                        key={j}
-                        className={`w-[35px] h-[35px] 
-                                            text-center font-medium
-                                            border rounded  
-                                            flex items-center justify-center  
-                                            cursor-pointer
-                                            transition-all duration-500 `}>
-                        {i}
-                    </div>)
+            <DaysOfWeek selectedDays={getSelectedDays(week_days)}/>
+            {/*{*/}
+            {/*    daysOfWeek.map((i, j) => <div*/}
+            {/*        key={j}*/}
+            {/*        className={`w-[35px] h-[35px] */}
+            {/*                            text-center font-medium*/}
+            {/*                            border rounded  */}
+            {/*                            flex items-center justify-center  */}
+            {/*                            cursor-pointer*/}
+            {/*                            transition-all duration-500 `}>*/}
+            {/*        {i}*/}
+            {/*    </div>)*/}
 
-                }
-            </div>
+            {/*}*/}
         </div>
     </div>
 }
