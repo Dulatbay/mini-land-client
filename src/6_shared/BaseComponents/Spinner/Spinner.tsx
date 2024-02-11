ce980a6d9913da9cd3a1307c05908e0c78bf3a6e
Dulatbay <adulatbai@bk.ru>
2024-02-11 10:21:05 +0600

    fix: change spinner

--- src/6_shared/BaseComponents/Spinner/Spinner.tsx
+++ src/6_shared/BaseComponents/Spinner/Spinner.tsx
@@ -1,6 +1,7 @@
 import './Spinner.css'
+
 export const Spinner = () => {
-    return <div className={'h-screen flex items-center bg-gray-700 justify-center'}>
+    return <div className={'h-full w-full  flex items-center justify-center'}>
         <div className={'loaderSpinner'}></div>
     </div>
 }
