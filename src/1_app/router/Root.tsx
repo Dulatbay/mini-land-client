17c518290abcb860a6619be1dd5768dc183ba509
Dulatbay <adulatbai@bk.ru>
2024-02-11 10:25:45 +0600

    fix: a bit of changes in root(change navbar routes), remove unused params in TimePicker.tsx

--- src/1_app/router/Root.tsx
+++ src/1_app/router/Root.tsx
@@ -1,6 +1,6 @@
-import {NavBar} from "@/3_widgets/NavBar";
 import {Outlet} from "react-router-dom";
 import {PrivateRoute} from "@/1_app/router/PrivateRoute.tsx";
+import {NavBar} from "@/3_widgets/NavBar/NavBar.tsx";
 
 export const Root = () => {
     return <PrivateRoute>
--- src/4_features/TimePicker/TimePicker.tsx
+++ src/4_features/TimePicker/TimePicker.tsx
@@ -1,18 +1,45 @@
+import React, {useEffect, useState} from "react";
+
 const availableHours = [
     8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
 ]
 
-export const TimePicker = () => {
-    return <div className="inline-flex text-lg border rounded-md p-1">
-        <select className="px-1 outline-none appearance-none bg-transparent">
-            {
-                availableHours.map((i, j) => <option value={`${j}`}>{i}</option>)
-            }
-        </select>
-        <span className="px-2">:</span>
-        <select name="" id="" className="px-1 outline-none appearance-none bg-transparent">
-            <option value="30">30</option>
-            <option value="00">00</option>
-        </select>
-    </div>
+interface Props {
+    onChange: React.Dispatch<React.SetStateAction<string | undefined>>,
+}
+
+export const TimePicker = ({onChange}: Props) => {
+    const [hour, setHour] = useState(12)
+    const [minute, setMinute] = useState(0)
+
+
+    useEffect(() => {
+        const formatNumber = (num : number) => {
+            return num < 10 ? `0${num}` : num;
+        };
+
+        const formattedTime = `${formatNumber(hour)}:${formatNumber(minute)}`;
+
+        onChange(formattedTime);
+    }, [hour, minute, onChange]);
+
+    return (
+        <div className="inline-flex text-lg border rounded-md p-1">
+            <select className="px-1 outline-none appearance-none bg-transparent"
+                    defaultValue={`${hour}`}
+                    onChange={(e) => setHour(+e.target.value)}
+            >
+                {
+                    availableHours.map((i,) => <option value={`${i}`} defaultChecked={i == hour} key={i}>{i}</option>)
+                }
+            </select>
+            <span className="px-2">:</span>
+            <select className="px-1 outline-none appearance-none bg-transparent"
+                    defaultValue={`${minute}`}
+                    onChange={(e) => setMinute(+e.target.value)}>
+                <option value="30">30</option>
+                <option value="0">00</option>
+            </select>
+        </div>
+    )
 }
