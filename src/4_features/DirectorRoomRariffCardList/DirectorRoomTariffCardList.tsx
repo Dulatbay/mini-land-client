232e0ec141af9c89d8761722175c78b2091c3e68
Dulatbay <adulatbai@bk.ru>
2024-02-11 10:25:02 +0600

    add: create DirectorRoomTariffCardList.tsx

--- src/4_features/DirectorRoomRariffCardList/DirectorRoomTariffCardList.tsx
+++ src/4_features/DirectorRoomRariffCardList/DirectorRoomTariffCardList.tsx
@@ -1,10 +1,27 @@
+import {useAllRoomTariffsQuery} from "@/5_entities/roomTariff/api/roomTariffApi.ts";
+import {Spinner} from "@/6_shared/BaseComponents/Spinner/Spinner.tsx";
+import {useEffect} from "react";
+import {getToastMessage} from "@/6_shared/lib/getToastMessage.ts";
 import {DirectorRoomTariffCard} from "@/5_entities/roomTariff/ui/DirectorRoomTariffCard.tsx";
 
 export const DirectorRoomTariffCardList = () => {
+    const {data, isError, error, isLoading} = useAllRoomTariffsQuery(true)
+
+    useEffect(() => {
+        if (isError)
+            getToastMessage(error)
+    }, [isError, error]);
+
+    if (isLoading)
+        return <Spinner/>
+
+    if (isError)
+        return <p className={'m-6 text-gray-700'}>Что-то пошло не так</p>
+
     return <div className={'flex flex-wrap mt-2 gap-2'}>
-        <DirectorRoomTariffCard/>
-        <DirectorRoomTariffCard/>
-        <DirectorRoomTariffCard/>
-        <DirectorRoomTariffCard/>
+        {
+            data!.length ? data!.map(i => <DirectorRoomTariffCard {...i} key={i.id}/>) :
+                <p className={'text-gray-700'}>Создайте ваш первый тариф</p>
+        }
     </div>
 }
