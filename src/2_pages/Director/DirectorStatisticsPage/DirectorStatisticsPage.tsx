6d10515ed2e32f8bccaf7c479c267326536cc6ee
Dulatbay <adulatbai@bk.ru>
2024-02-16 11:58:03 +0600

    add: create ProfitCard.tsx

--- src/2_pages/Director/DirectorStatisticsPage/DirectorStatisticsPage.tsx
+++ src/2_pages/Director/DirectorStatisticsPage/DirectorStatisticsPage.tsx
@@ -1,11 +1,11 @@
 import {PieChartContainer} from "@/3_widgets/PieChartContainer/PieChartContainer.tsx";
-import {PlotChartContainer} from "@/3_widgets/PlotChartContainer/PlotChartContainer.tsx";
+import {ProfitsList} from "@/3_widgets/ProfitsList/ProfitsList.tsx";
 
 export const DirectorStatisticsPage = () => {
     return <>
         <div className={'w-[95%] m-auto flex flex-row justify-center gap-4'}>
             <PieChartContainer/>
-            <PlotChartContainer/>
+            <ProfitsList/>
         </div>
     </>
 }
---
+++ src/4_features/ProfitCard/ProfitCard.tsx
@@ -0,0 +1,7 @@
+
+export const ProfitCard = () => {
+    return <div className={'bg-gray-50 w-full p-4 rounded border-red-700 border-2'}>
+        <p>Расход</p>
+        <p>13000 тг</p>
+    </div>
+}
