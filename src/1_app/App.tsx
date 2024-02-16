46ce4daedbcaac25f0ca66c14d849a7c3f5693cd
Dulatbay <adulatbai@bk.ru>
2024-02-16 11:51:27 +0600

    fix: auth error

--- src/1_app/App.tsx
+++ src/1_app/App.tsx
@@ -1,7 +1,19 @@
 import {Bounce, ToastContainer} from "react-toastify";
+import {useContext, useEffect} from "react";
+import KeycloakContext from "@/1_app/keycloak/KeycloakContext.ts";
 
 
 function App() {
+    const keycloak = useContext(KeycloakContext);
+
+    useEffect(() => {
+        if (keycloak.isTokenExpired() || !keycloak.authenticated)
+            localStorage.removeItem('token')
+        else {
+            localStorage.setItem('token', keycloak.token!)
+        }
+    }, [keycloak])
+
     return (
         <>
             <ToastContainer position="bottom-right"
--- src/1_app/router/AppRouter.tsx
+++ src/1_app/router/AppRouter.tsx
@@ -1,4 +1,4 @@
-import {useContext, useEffect} from "react";
+import {useContext} from "react";
 import KeycloakContext from "@/1_app/keycloak/KeycloakContext.ts";
 import {directorRouter} from "@/1_app/router/directorRouter.tsx";
 import {managerRouter} from "@/1_app/router/managerRouter.tsx";
@@ -6,11 +6,6 @@ import {managerRouter} from "@/1_app/router/managerRouter.tsx";
 
 export const AppRouter = () => {
     const keycloak = useContext(KeycloakContext);
-
-    useEffect(() => {
-        localStorage.setItem('token', keycloak.token!)
-    }, [keycloak])
-
     const isAdmin = keycloak.hasResourceRole("ADMIN")
 
     if (isAdmin)
