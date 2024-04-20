cc3366874402567d02a03a6f2991e902fa1a935c
qqanly <adulatbai@bk.ru>
2024-04-20 16:44:09 +0600

    Update API and Keycloak URLs
    The changes in this commit update the API and Keycloak URL settings in the .env file. They have been modified to reflect new server addresses. Also, a small adjustment was made in the keycloak configuration file to correctly use the updated URL.

--- src/1_app/keycloak/kc-config.ts
+++ src/1_app/keycloak/kc-config.ts
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const initOptions = {
-    url: `${import.meta.env.VITE_KEYCLOAK_URL}/auth`,
+    url: `${import.meta.env.VITE_KEYCLOAK_URL}`,
     realm: 'miniland',
     clientId: 'miniland',
 }
